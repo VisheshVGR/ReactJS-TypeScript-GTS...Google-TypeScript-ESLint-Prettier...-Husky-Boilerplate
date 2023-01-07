b1c633c6306b9da88151546795db42430d87c57b
Vishesh Gupta <visheshguptavgr@gmail.com>
2023-01-08 01:31:31 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ReactJS + TypeScript + GTS (Google TypeScript + ESLint + Prettier) + Husky Boilerplate
+
+```
+Will get updated after testing.
+```
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>
+        ReactJS + TypeScript + GTS (Google TypeScript + ESLint + Prettier) +
+        Husky Boilerplate
+      </h1>
+    </>
+  );
+};
+
+export default App;
